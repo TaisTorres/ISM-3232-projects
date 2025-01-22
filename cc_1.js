bbab8e0b585583d064bfb412ffcb45a9ab083bb8
Tais Torres <taisincollege@gmail.com>
2025-01-21 22:08:22 -0500

    Task 1 - Employee Information.

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,6 @@
-let employeeName 
+let employeeName = "Jane Doe";
+const employeeID = 123;
+var isActive = true;
+console.log(typeof "Jane Doe");
+console.log(typeof 123);
+console.log(typeof true);
